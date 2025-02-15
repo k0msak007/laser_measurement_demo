6303c26f1ccf34f8e569aa7b20830b7e6ba5ee44
komsak007 <komsak@iconframework.com>
2025-02-15 23:14:23 +0700

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Laser_Measurement_client/",
-  build: {
-    outDir: "dist", // หรือ public แล้วแต่ Netlify config
-  },
+  base: "/laser_measurement_demo/",
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
